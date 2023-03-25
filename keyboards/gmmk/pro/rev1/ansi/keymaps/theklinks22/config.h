5c26d5326b32fa7b57a996b3efadc167ffac2006
Andrew <klinks22@gmail.com>
2023-03-25 16:48:06 -0400

    Turn off led when computer is idle/off

--- keyboards/gmmk/pro/rev1/ansi/keymaps/theklinks22/config.h
+++ keyboards/gmmk/pro/rev1/ansi/keymaps/theklinks22/config.h
@@ -1,6 +1,7 @@
-// #pragma once
+#pragma once
 
-// #ifdef RGB_MATRIX_ENABLE
-//     #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
-//     #define RGB_DISABLE_WHEN_USB_SUSPENDED
-// #endif
+#ifdef RGB_MATRIX_ENABLE
+    #define RGB_MATRIX_DEFAULT_MODE RGB_MATRIX_SOLID_COLOR
+    #define RGB_DISABLE_WHEN_USB_SUSPENDED
+#endif
+
